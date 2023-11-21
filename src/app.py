842ea6ca38122e8eaf9ef4d0328e8d9b2048d65f
Frankkmansilla <frankmansilla123a@gmail.com>
2023-11-20 21:40:36 -0300

    add test-port 003

--- src/app.py
+++ src/app.py
@@ -1,17 +1,15 @@
 from flask import Flask, jsonify
 from users import users
 
-
 app = Flask(__name__)
 
-@app.route('/', methods=['get'])
+@app.route('/', methods=['GET'])
 def ping():
-    return jsonify({"response": "hellow word"})
+    return jsonify({"response": "Hello World"})
 
 @app.route('/users')
 def usersHandler():
     return jsonify({"users": users})
 
-
-if __name__=='__main__':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, port=3359)  # Se inicializa en el puerto 3359
